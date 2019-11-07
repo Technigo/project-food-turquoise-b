b295bc1b5d512a75dd3a3d9e5d6b34cfcfd9112d
Nina Månsson <ninamansster@gmail.com>
2019-11-07 16:32:15 +0100

    mobile first, font style

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ fetch(url, { headers: { "user-key": apiKey } })
       //console.log(resto.restaurant.name)
       document.getElementById("restaurants").innerHTML +=
         `<div class="cards"><img class="image" src=${resto.restaurant.photos[0].photo.url}>
-      <li>${resto.restaurant.name} </li>
-      <li>${resto.restaurant.average_cost_for_two} €</li>
-      <li>${resto.restaurant.location.address}</li>
-      <li>${resto.restaurant.user_rating.aggregate_rating}</li></div>`
+      <li><h2>${resto.restaurant.name}</h2> </li>
+      <li><h3>Price: ${resto.restaurant.average_cost_for_two} ${resto.restaurant.currency}</h3></li>
+      <li><h4>Adress: ${resto.restaurant.location.address}</h4></li>
+      <li><h5>Rating: ${resto.restaurant.user_rating.aggregate_rating} 🐙<h5></li></div>`
     });
   });
 
