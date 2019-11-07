650d34e24dedda7f10862c96be8098075eb4bef3
Nina Månsson <ninamansster@gmail.com>
2019-11-07 15:15:01 +0100

    added css-file

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ fetch(url, { headers: { "user-key": apiKey } })
     json.restaurants.forEach((resto) => {
       //console.log(resto.restaurant.name)
       document.getElementById("restaurants").innerHTML +=
-        `<img src=${resto.restaurant.photos[0].photo.thumb_url}> 
-      <li>${resto.restaurant.name} </li> 
-      <li>${resto.restaurant.average_cost_for_two} €</li> 
-      <li>${resto.restaurant.location.address}</li> 
-      <li>${resto.restaurant.user_rating.aggregate_rating}</li>`
+        `<div class="cards"><img src=${resto.restaurant.photos[0].photo.thumb_url}>
+      <li>${resto.restaurant.name} </li>
+      <li>${resto.restaurant.average_cost_for_two} €</li>
+      <li>${resto.restaurant.location.address}</li>
+      <li>${resto.restaurant.user_rating.aggregate_rating}</li></div>`
     });
   });
 
