19cb7336f69a7e85b395bbde96789bde352a9b71
Frida Jonsson <annafridamariajonsson@gmail.com>
2019-11-08 11:11:20 +0100

    Created additional filter functions  for price range & restore filters

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&e
 
 // ARRAY for json from API
 let restaurantList = []
+// HTML DIV FOR RESTAURANTS
 
 
 fetch(url, { headers: { "user-key": apiKey } })
@@ -29,15 +30,67 @@ fetch(url, { headers: { "user-key": apiKey } })
  // Filter on price range
 
  document.getElementById("price-3").addEventListener('click', () => {
-   renderRestaurants()
+   filter3()
  })
 
-  const renderRestaurants = () => {
+  const filter3 = () => {
+    // Null all restaurants
+    document.getElementById("restaurants").innerHTML=""
     restaurantList.forEach((resto) => {
-      if (resto.restaurant.price_range !== 3) {
-        console.log("hide")
+      if (resto.restaurant.price_range == 3) {
+        //show restaurants if price range = 3
+        document.getElementById("restaurants").innerHTML +=
+        `<img src=${resto.restaurant.photos[0].photo.thumb_url}> 
+        <li>${resto.restaurant.name} </li> 
+        <li>${resto.restaurant.average_cost_for_two} €</li> 
+      <li>${resto.restaurant.location.address}</li> 
+      <li>${resto.restaurant.user_rating.aggregate_rating}</li>
+      <li>${resto.restaurant.price_range}</li>`
         } else {
-        console.log ("show")
+          // don't show other restaurants
+          return
         }
+    })
+  }
+
+  document.getElementById("price-4").addEventListener('click', () => {
+    filter4()
+  })
+
+  const filter4 = () => {
+    // Null all restaurants
+    document.getElementById("restaurants").innerHTML=""
+    restaurantList.forEach((resto) => {
+      if (resto.restaurant.price_range == 4) {
+        //show restaurants if price range = 4
+        document.getElementById("restaurants").innerHTML +=
+        `<img src=${resto.restaurant.photos[0].photo.thumb_url}> 
+        <li>${resto.restaurant.name} </li> 
+        <li>${resto.restaurant.average_cost_for_two} €</li> 
+      <li>${resto.restaurant.location.address}</li> 
+      <li>${resto.restaurant.user_rating.aggregate_rating}</li>
+      <li>${resto.restaurant.price_range}</li>`
+        } else {
+          // don't show other restaurants
+          return
+        }
+    })
+  }
+  document.getElementById("restore-filter").addEventListener('click', () => {
+    restoreFilter()
+  })
+
+  const restoreFilter = () => {
+    // Null all restaurants
+    document.getElementById("restaurants").innerHTML=""
+    restaurantList.forEach((resto) => {
+        //show all restaurants 
+        document.getElementById("restaurants").innerHTML +=
+        `<img src=${resto.restaurant.photos[0].photo.thumb_url}> 
+        <li>${resto.restaurant.name} </li> 
+        <li>${resto.restaurant.average_cost_for_two} €</li> 
+      <li>${resto.restaurant.location.address}</li> 
+      <li>${resto.restaurant.user_rating.aggregate_rating}</li>
+      <li>${resto.restaurant.price_range}</li>`
     })
   }
